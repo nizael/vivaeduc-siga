582860b5562204619aad913ec5c2e43c49c94e36
nizael <nizael@nizael.com.br>
2024-11-01 11:27:20 -0300

    feat: add favicon

--- src/common/components/_layout/LayoutWeb.tsx
+++ src/common/components/_layout/LayoutWeb.tsx
@@ -3,6 +3,7 @@ import { Aside } from "./Aside"
 import { Header } from "./Header"
 import { guardian } from "../../../di/dependencyInjection"
 import { IUserData } from "../../../app/(auth)/stores/useAuthData"
+import Head from "next/head"
 
 type ILayoutWebProps = PropsWithChildren & {
   titlePage: ReactNode
@@ -10,12 +11,17 @@ type ILayoutWebProps = PropsWithChildren & {
 export const LayoutWeb = async ({ titlePage, children }: ILayoutWebProps) => {
   const authData = await guardian.authenticate()
   return (
-    <div className="grid grid-cols-[250px,1fr] grid-rows-[66px,1fr]  w-full h-full">
-      <Aside />
-      <Header titlePage={titlePage} authData={authData} />
-      <main className="flex flex-col gap-4 overflow-y-auto p-4">
-        {children}
-      </main>
-    </div>
+    <>
+      <Head>
+        <link rel="icon" href="/svg/logo_48.svg" type="image/x-icon" />
+      </Head>
+      <div className="grid grid-cols-[250px,1fr] grid-rows-[66px,1fr]  w-full h-full">
+        <Aside />
+        <Header titlePage={titlePage} authData={authData} />
+        <main className="flex flex-col gap-4 overflow-y-auto p-4">
+          {children}
+        </main>
+      </div>
+    </>
   )
 }
