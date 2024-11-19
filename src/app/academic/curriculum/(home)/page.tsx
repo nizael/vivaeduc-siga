270ae9f4193be8b215d2df0f21e7467abfe6fb31
nizael <nizael@nizael.com.br>
2024-11-19 20:46:17 -0300

    feat: add EmptyPage in CurriculumPage

--- src/app/academic/curriculum/(home)/page.tsx
+++ src/app/academic/curriculum/(home)/page.tsx
@@ -4,6 +4,7 @@ import { ListCurriculum } from "./components/ListCurriculum";
 import { ToolBar } from "./components/ToolBar";
 import { CreateCurriculum } from "../create/CreateCurriculum";
 import { listAllCurriculums } from "@/services/curriculum/curriculumGets";
+import { EmptyPage } from "@/components/empty-state/EmptyPage";
 
 export default async function CurriculumPage() {
   const { data, status } = await listAllCurriculums()
@@ -14,7 +15,7 @@ export default async function CurriculumPage() {
         <div className="flex flex-col gap-4 p-4 min-h-full">
           <TitlePage title="Grades currículares" />
           <ToolBar />
-          {(status === 200 && data) && <ListCurriculum listCurriculum={data} />}
+          {(status === 200 && data) ? <ListCurriculum listCurriculum={data} />: <EmptyPage label="Nenhuma grade curricular encontrada"/>}
         </div>
       </LayoutApp>
     </>
